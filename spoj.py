ef4aa265a86bb13f57320c6f007341b085b46ec4
Paul Draper <paul@lucidchart.com>
2015-11-06 09:12:38 -0700

    Add contest footer to spoj.py

--- spoj.py
+++ spoj.py
@@ -5,10 +5,6 @@ import mechanize
 import requests
 import sys
 
-class AuthenticationError(Exception):
-    def __init__(self):
-        Exception.__init__(self)
-
 class SPOJ:
     def __init__(self):
         self.base_url = 'http://www.spoj.com'
@@ -28,9 +24,20 @@ class SPOJ:
         self.browser.submit()
         cookies = self.browser._ua_handlers['_cookies'].cookiejar
         if not any(c.name == 'SPOJ' for c in cookies):
-            print 'Authentication failed'
-            raise AuthenticationError()
+            raise Exception('Authentication failed')
         print 'Authenticated'
+
+    def edit_contest(self, contest_code, footer_path=None):
+        self.navigate('{}/{}/edit'.format(self.base_url, contest_code))
+        self.browser.select_form(nr=0)
+        if footer_path is not None:
+            print 'Changing contest footer to {}'.format(footer_path)
+            with open(footer_path, 'r') as footer_file:
+                self.browser['body'] = footer_file.read()
+
+        print 'Editing {}'.format(contest_code)
+        self.browser.submit()
+        print 'Edited {}'.format(contest_code)
 
     def edit_problem(self, problem_code, body_path=None):
         self.navigate('{}/problems/{}/edit'.format(self.base_url, problem_code))
@@ -44,12 +51,14 @@ class SPOJ:
         self.browser.submit()
         print 'Edited {}'.format(problem_code)
 
+def contest_main(args):
+    spoj = SPOJ()
+    spoj.authenticate(args.user, args.password)
+    spoj.edit_contest(args.contest_code, footer_path=args.footer_file)
+
 def problem_main(args):
     spoj = SPOJ()
-    try:
-        spoj.authenticate(args.user, args.password)
-    except AuthenticationError:
-        sys.exit(1)
+    spoj.authenticate(args.user, args.password)
     spoj.edit_problem(args.problem_code, body_path=args.body_file)
 
 if __name__ == '__main__':
@@ -58,10 +67,15 @@ if __name__ == '__main__':
     parser.add_argument('--password')
     subparsers = parser.add_subparsers()
 
-    parser_problem = subparsers.add_parser('problem')
-    parser_problem.add_argument('problem_code', help='Alphanumeric problem code')
-    parser_problem.add_argument('--body-file', help='Problem description file')
-    parser_problem.set_defaults(func=problem_main)
+    subparser = subparsers.add_parser('contest')
+    subparser.add_argument('contest_code', help='Alphanumeric contest code')
+    subparser.add_argument('--footer-file', help='Footer HTML file')
+    subparser.set_defaults(func=contest_main)
+
+    subparser = subparsers.add_parser('problem')
+    subparser.add_argument('problem_code', help='Alphanumeric problem code')
+    subparser.add_argument('--body-file', help='Problem description file')
+    subparser.set_defaults(func=problem_main)
 
     args = parser.parse_args()
     args.func(args)
